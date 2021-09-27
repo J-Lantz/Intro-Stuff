714be25e087b33ba84b8ea54a51a43ba6b5c4ea0
J_Lantz <lantzjf2@miamioh.edu>
2021-09-27 13:00:23 -0400

    yes

---
+++ r code.R
@@ -0,0 +1,17 @@
+
+
+installr::install.git()
+install.packages("installr")
+updateR()
+
+
+install.packages("usethis")
+
+
+usethis::use_git_config(scope = "user", # or "project" to for a given repo
+                        user.name = "J_Lantz", # replace with your GitHub User Name
+                        user.email = "lantzjf2@miamioh.edu") # replace with your GitHub email
+
+
+#need to install git and other version, use where git, 
+#J: file location/git init
